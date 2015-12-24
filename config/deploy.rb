60e5a1fb10235f1c90a6a3edf63d07f12b8d286e
abc1 <abc1@abc1-desktop>
2015-12-24 16:53:15 +0530

    Modified Capfile

--- config/deploy.rb
+++ config/deploy.rb
@@ -18,9 +18,9 @@ set :use_sudo, false
 #set :deploy_via, :remote_cache
 
 task :production do
-	role :web, "67.206.189.228"
-	role :app, "67.206.189.228"
-	role :db, "67.206.189.228", :primary=>true
+	role :web, "root@67.206.189.228"
+	role :app, "root@67.206.189.228"
+	role :db, "root@67.206.189.228", :primary=>true
 
 	set :deploy_to, '/var/www/home/technostacks/public_html/demo/demoapp'
 	set :user, "root"
