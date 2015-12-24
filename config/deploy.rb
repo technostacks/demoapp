4851940ffc38319cb66ab253ad73c3d56a23a806
abc1 <abc1@abc1-desktop>
2015-12-24 12:54:22 +0530

    Modified Capfile

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,8 +2,8 @@
 lock '3.4.0'
 
 set :application, 'demoapp'
-set :repo_url, 'https://github.com/technostacks/demoapp.git'
-set :deploy_to, '/var/www/home/technostacks/public_html/demo/dempapp'
+set :repository, 'git@github.com:technostacks/demoapp.git'
+set :deploy_to, '/var/www/home/technostacks/public_html/demo/demoapp'
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
@@ -11,17 +11,17 @@ set :deploy_to, '/var/www/home/technostacks/public_html/demo/dempapp'
 # Default deploy_to directory is /var/www/my_app_name
 # set :deploy_to, '/var/www/my_app_name'
 
-# Default value for :scm is :git
-set :scm, :git
+# Default value for :scm is :git, type of repository
+# set :scm, :git
 
 # Default value for :format is :pretty
-set :format, :pretty
+# set :format, :pretty
 
 # Default value for :log_level is :debug
 set :log_level, :debug
 
 # Default value for :pty is false
-set :pty, true
+# set :pty, true
 
 # Default value for :linked_files is []
 # set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
