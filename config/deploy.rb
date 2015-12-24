c13f600141288357c56a8f836b244c9ed593ae95
abc1 <abc1@abc1-desktop>
2015-12-24 17:05:16 +0530

    Modified Capfile

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,34 +1,66 @@
+# config valid only for current version of Capistrano
+lock '3.4.0'
+
 set :application, "demoapp"
-set :repository,  "https://github.com/technostacks/demoapp.git"
- 
-set :user, :root
- 
-set :deploy_to, "/var/www/home/technostacks/public_html/demo/demoapp"
- 
-set :use_sudo, false
- 
+set :repository, 'git@github.com:technostacks/demoapp.git'
+
+
+
+# Default branch is :master
+# ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
+
+# Default deploy_to directory is /var/www/my_app_name
+# set :deploy_to, '/var/www/my_app_name'
+
+# Default value for :scm is :git, type of repository
 set :scm, :git
- 
-role :web, "67.206.189.228"                          # Your HTTP server, Apache/etc
-role :app, "67.206.189.228"                          # This may be the same as your `Web` server
-role :db,  "67.206.189.228", :primary => true # This is where Rails migrations will run
+set :use_sudo, false
+set :deploy_via, :copy
 
- 
+task :production do
+	role :web, "root@67.206.189.228"
+	role :app, "root@67.206.189.228"
+	role :db, "root@67.206.189.228", :primary=>true
 
- 
+	set :deploy_to, '/var/www/home/technostacks/public_html/demo/demoapp'
+	set :user, "root"
+end
+
+#after "deploy:symlink", "deploy:link_config"
+#after "deploy:link_config", "deploy:migrate"
+
+
+
+# Default value for :format is :pretty
+set :format, :pretty
+
+# Default value for :log_level is :debug
+set :log_level, :debug
+
+# Default value for :pty is false
+set :pty, true
+
+# Default value for :linked_files is []
+# set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
+
+# Default value for linked_dirs is []
+# set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
+
+# Default value for default_env is {}
+# set :default_env, { path: "/opt/ruby/bin:$PATH" }
+
+# Default value for keep_releases is 5
+# set :keep_releases, 5
+
 namespace :deploy do
-   task :start do ; end
-   task :stop do ; end
-   task :restart, :roles => :app, :except => { :no_release => true } do
-     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
-   end
- 
-   desc "Installs required gems"
-   task :gems, :roles => :app do
-     run "cd #{current_path} && sudo rake gems:install RAILS_ENV=production"
-   end
-   after "deploy:setup", "deploy:gems"  
- 
-   before "deploy", "deploy:web:disable"
-   after "deploy", "deploy:web:enable"
+
+  after :restart, :clear_cache do
+    on roles(:web), in: :groups, limit: 3, wait: 10 do
+      # Here we can do anything such as:
+      # within release_path do
+         execute :rake, 'cache:clear'
+      # end
+    end
+  end
+
 end
