df831f54a0017cf3a173f50d79cca14f90840427
abc1 <abc1@abc1-desktop>
2015-12-24 18:07:19 +0530

    Modified Capfile

--- config/deploy.rb
+++ config/deploy.rb
@@ -15,14 +15,14 @@ set :scm, :git
 set :use_sudo, false
 set :deploy_via, :copy
 
-task :production do
-	role :web, "root@67.206.189.228"
-	role :app, "root@67.206.189.228"
-	role :db, "root@67.206.189.228", :primary=>true
+#task :production do
+role :web, "root@67.206.189.228"
+role :app, "root@67.206.189.228"
+role :db, "root@67.206.189.228", :primary=>true
 
-	set :deploy_to, '/var/www/home/technostacks/public_html/demo/demoapp'
-	set :user, "root"
-end
+set :deploy_to, '/var/www/home/technostacks/public_html/demo/demoapp'
+set :user, "root"
+#end
 
 #after "deploy:symlink", "deploy:link_config"
 #after "deploy:link_config", "deploy:migrate"
