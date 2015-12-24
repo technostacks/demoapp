ea70b819b848db60629c4297e920366477bebd50
abc1 <abc1@abc1-desktop>
2015-12-24 10:54:43 +0530

    Modified Capfile

--- config/deploy.rb
+++ config/deploy.rb
@@ -12,16 +12,16 @@ set :deploy_to, '/var/www/home/technostacks/public_html/demo/dempapp'
 # set :deploy_to, '/var/www/my_app_name'
 
 # Default value for :scm is :git
-# set :scm, :git
+set :scm, :git
 
 # Default value for :format is :pretty
-# set :format, :pretty
+set :format, :pretty
 
 # Default value for :log_level is :debug
-# set :log_level, :debug
+set :log_level, :debug
 
 # Default value for :pty is false
-# set :pty, true
+set :pty, true
 
 # Default value for :linked_files is []
 # set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
